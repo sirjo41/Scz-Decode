8cb13b76f96802e19c111d13ad79bd22e7272a07
siraj <srjbadri2008@gmail.com>
2026-01-19 19:17:21 +0200

    shoot 4

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ColorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ColorTest.java
@@ -5,23 +5,29 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "ColorTest")
 public class ColorTest extends OpMode {
 
-    DcMotor spindexer;
+    Servo ss;
 
     @Override
     public void init() {
-        spindexer = hardwareMap.get(DcMotor.class, "spindexer");
+        ss = hardwareMap.get(Servo.class, "feeder");
+        ss.setPosition(1);
     }
 
 
     @Override
     public void loop() {
-        spindexer.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        if (gamepad1.a){
+            ss.setPosition(0.55);
+        } else if (gamepad1.b) {
+            ss.setPosition(1);
+        }
 
-        telemetry.addData("spin",spindexer.getCurrentPosition());
+        telemetry.addData("spin",ss.getPosition());
         telemetry.update();
     }
 }
