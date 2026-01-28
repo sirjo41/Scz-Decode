db58717bc080b7dedd28f3c9ee55761da39f16b9
siraj <srjbadri2008@gmail.com>
2026-01-28 18:15:46 +0200

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ColorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ColorTest.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
+@TeleOp(name = "Colortest")
 public class ColorTest extends LinearOpMode {
 
 
